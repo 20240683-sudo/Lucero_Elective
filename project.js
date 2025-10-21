01aefc8652c33f2833d504e61c4cb729ec11bcc3
20240683_sudo <20240683@nbsc.edu.ph>
2025-10-21 13:06:08 +0800

    enhancing project

--- project.js
+++ project.js
@@ -1,6 +1,8 @@
 const button = document.getElementById('colorButton');
 
-button.addEventListener('click', function() {
+if (button) {
+  button.addEventListener('click', () => {
     document.body.style.backgroundColor =
-        document.body.style.backgroundColor === 'lightyellow' ? '#f4f4f9' : 'lightyellow';
-});
+      document.body.style.backgroundColor === 'lightyellow' ? '#f4f4f9' : 'lightyellow';
+  });
+}
